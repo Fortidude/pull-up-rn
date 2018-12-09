2821c8b9c95ac4d02fbe843ce1b892a4b35b467d
Artur Schroeder <artur@fachowo.co>
2018-12-09 21:00:43 +0100

    fix font sizes

--- src/service/helpers/DetermineDevice.tsx
+++ src/service/helpers/DetermineDevice.tsx
@@ -3,7 +3,7 @@ import { Dimensions, Platform } from "react-native";
 const { height, width } = Dimensions.get('window');
 
 const isIphoneX = () => (Platform.OS === 'ios' && (height === 812 || width === 812));
-const isSmallScreen = () => (height < 400) || (width < 400);
+const isSmallScreen = () => (height < 350) || (width < 350);
 
 export default {
     isIphoneX,
